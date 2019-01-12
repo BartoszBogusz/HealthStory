e14c788061780068f85529f2f07fefe377370104
Tomasz Krupa <etomaszkrupa@gmail.com>
2019-01-12 15:35:46 +0100

    Added AppUser entity

---
+++ HealthStory.Web/HealthStory.Web/Entities/AppUser.cs
@@ -0,0 +1,10 @@
+﻿namespace HealthStory.Web.Entities
+{
+    public class AppUser
+    {
+        public int UserId { get; set; }
+        public string Login { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
