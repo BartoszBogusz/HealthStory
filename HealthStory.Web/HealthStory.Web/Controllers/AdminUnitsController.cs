73d4812537b3372496a896e719e6c3854f272595
nithaltak <42481119+nithaltak@users.noreply.github.com>
2019-01-14 15:48:29 +0100

    Added showing units

--- HealthStory.Web/HealthStory.Web/Controllers/AdminUnitsController.cs
+++ HealthStory.Web/HealthStory.Web/Controllers/AdminUnitsController.cs
@@ -2,21 +2,27 @@
 using HealthStory.Web.Infrastructure;
 using HealthStory.Web.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
 
 namespace HealthStory.Web.Controllers
 {
     public class AdminUnitsController : Controller
     {
         private readonly IAdminUnitsResolver _adminUnitsResolver;
+        private readonly HealthStoryContext _healthStoryContext;
 
-        public AdminUnitsController(IAdminUnitsResolver adminUnitsResolver)
+        public AdminUnitsController(IAdminUnitsResolver adminUnitsResolver, HealthStoryContext healthStoryContext)
         {
             _adminUnitsResolver = adminUnitsResolver;
+            _healthStoryContext = healthStoryContext;
         }
 
+        
+ 
         public ActionResult Index()
         {
-            return View();
+            return View(_healthStoryContext.Units.ToList());
         }
 
         public IActionResult Create()
@@ -28,7 +34,7 @@ namespace HealthStory.Web.Controllers
         public IActionResult AddUnit(AdminUnitsDto model)
         {
             _adminUnitsResolver.AddUnit(model);
-            return View("Index");
+            return RedirectToAction("Index", "AdminUnits");
         }
     }
 }
