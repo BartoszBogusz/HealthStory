df54d5405f4ed78cdc222a394e70490fcff97beb
nithaltak <42481119+nithaltak@users.noreply.github.com>
2019-01-17 10:25:36 +0100

    Added SubstanceDefinitionReadModel

---
+++ HealthStory.Web/HealthStory.Web/Models/SubstanceDefinition/SubstanceDefinitionReadModel.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+
+namespace HealthStory.Web.Models.SubstanceDefinition
+{
+    public class SubstanceDefinitionReadModel
+    {
+        public string Name { get; set; }
+        public decimal Max { get; set; }
+        public decimal Min { get; set; }
+        public string Unit { get; set; }
+    }
+}
