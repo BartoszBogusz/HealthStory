13e81d0f599e59fa73fa52a4e3e4705cc859f92d
nithaltak <42481119+nithaltak@users.noreply.github.com>
2019-01-26 18:11:40 +0100

    Added AppUserBloodTestValue entity

--- HealthStory.Web/HealthStory.Web/Infrastructure/HealthStoryContext.cs
+++ HealthStory.Web/HealthStory.Web/Infrastructure/HealthStoryContext.cs
@@ -10,6 +10,7 @@ namespace HealthStory.Web.Infrastructure
         }
 
         public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<AppUserBloodTestValue> AppUserBloodTestValues { get; set; }
         public DbSet<BloodTestInfo> BloodTestsInfo { get; set; }
         public DbSet<BloodTestSubstanceInfo> BloodTestsSubstancesInfo { get; set; }
         public DbSet<SubstanceInfo> SubstanceInfo { get; set; }
@@ -26,6 +27,46 @@ namespace HealthStory.Web.Infrastructure
                 .Property(f => f.AppUserId)
                 .ValueGeneratedOnAdd();
 
+            modelBuilder.Entity<AppUser>()
+                .HasMany(x => x.AppUserBloodTestValue)
+                .WithOne(x => x.AppUser)
+                .HasForeignKey(x => x.AppUserId);
+
+            // AppUserBloodTestValue
+            modelBuilder.Entity<AppUserBloodTestValue>()
+                .HasKey(x => x.AppUserBloodTestValueId);
+            modelBuilder.Entity<AppUserBloodTestValue>()
+                .Property(f => f.AppUserBloodTestValueId)
+                .ValueGeneratedOnAdd();
+
+            // BloodTestInfo
+            modelBuilder.Entity<BloodTestInfo>()
+                .HasKey(x => x.BloodTestInfoId);
+            modelBuilder.Entity<BloodTestInfo>()
+                .Property(f => f.BloodTestInfoId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<BloodTestInfo>()
+              .HasMany(x => x.AppUserBloodTestValue)
+              .WithOne(x => x.BloodTestInfo)
+              .HasForeignKey(x => x.BloodTestInfoId);
+
+            // BloodTestSubstanceInfo
+            modelBuilder.Entity<BloodTestSubstanceInfo>()
+                .HasKey(x => new { x.BloodTestInfoId, x.SubstanceInfoId });
+
+            // SubstanceInfo
+            modelBuilder.Entity<SubstanceInfo>()
+                .HasKey(x => x.SubstanceInfoId);
+            modelBuilder.Entity<SubstanceInfo>()
+                .Property(f => f.SubstanceInfoId)
+                .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<SubstanceInfo>()
+              .HasMany(x => x.AppUserBloodTestValue)
+              .WithOne(x => x.SubstanceInfo)
+              .HasForeignKey(x => x.SubstanceInfoId);
+
             //Units
             modelBuilder.Entity<Unit>()
                 .HasKey(x => x.UnitId);
@@ -37,25 +78,6 @@ namespace HealthStory.Web.Infrastructure
                 .HasMany(x => x.SubstanceInfo)
                 .WithOne(x => x.Unit)
                 .HasForeignKey(x => x.UnitId);
-
-            // SubstanceInfo
-            modelBuilder.Entity<SubstanceInfo>()
-                .HasKey(x => x.SubstanceInfoId);
-            modelBuilder.Entity<SubstanceInfo>()
-                .Property(f => f.SubstanceInfoId)
-                .ValueGeneratedOnAdd();
-
-            // BloodTestInfo
-            modelBuilder.Entity<BloodTestInfo>()
-                .HasKey(x => x.BloodTestInfoId);
-            modelBuilder.Entity<BloodTestInfo>()
-                .Property(f => f.BloodTestInfoId)
-                .ValueGeneratedOnAdd();
-
-            // BloodTestInfo
-            modelBuilder.Entity<BloodTestSubstanceInfo>()
-                .HasKey(x => new { x.BloodTestInfoId, x.SubstanceInfoId });
-
         }
     }
 }
