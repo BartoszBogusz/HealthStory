f1d41d9f3591f26e07384cbe649c72a09c034209
nithaltak <42481119+nithaltak@users.noreply.github.com>
2019-02-19 12:17:08 +0100

    Added Dashboard

---
+++ HealthStory.Web/HealthStory.Web/Controllers/DashboardController.cs
@@ -0,0 +1,23 @@
+﻿using HealthStory.Web.Application.AdminBloodTestInfo;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HealthStory.Web.Controllers
+{
+    [Authorize]
+    public class DashboardController : Controller
+    {
+        private readonly IAdminBloodTestInfoService _adminBloodTestService;
+
+        public DashboardController(IAdminBloodTestInfoService adminBloodTestInfoService)
+        {
+            _adminBloodTestService = adminBloodTestInfoService;
+        }
+
+        public IActionResult Index()
+        {
+            var model = _adminBloodTestService.Get();
+            return View(model);
+        }
+    }
+}
