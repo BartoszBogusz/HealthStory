275b4fb60fd8fb3d3d30c902c6faf5b425fe031f
nithaltak <42481119+nithaltak@users.noreply.github.com>
2019-02-21 18:37:18 +0100

    Added controller and view for BloodTest

---
+++ HealthStory.Web/HealthStory.Web/Controllers/BloodTestController.cs
@@ -0,0 +1,22 @@
+﻿using HealthStory.Web.Application.BloodTest.User;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace HealthStory.Web.Controllers
+{
+    public class BloodTestController : Controller
+    {
+        private readonly IUserBloodTestProvider _userBloodTestProvider;
+
+        public BloodTestController(IUserBloodTestProvider userBloodTestProvider)
+        {
+            _userBloodTestProvider = userBloodTestProvider;
+        }
+
+        public async Task<IActionResult> New(int bloodTestId)
+        {
+            var bloodTest = await _userBloodTestProvider.GetAsync(1);
+            return View(bloodTest);
+        }
+    }
+}
